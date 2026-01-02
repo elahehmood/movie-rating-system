cf1f18f6e51192a53044fac97502e69a6b53e8dd
elahehmood <elahe.mod004@gmail.com>
2026-01-02 22:54:58 +0330

    chore: add file logging configuration

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.controller.router import api_router
+from app.core.logging_config import setup_logging
 
+setup_logging()
 app = FastAPI()
 
 # Include API router
